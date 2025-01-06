57ee41232ac01f0aec5f9a01015c76408d89904d
mohamed-magdy-1 <King44123no@gamil.com>
2025-01-06 16:42:06 +0200

    new

--- config/database.js
+++ config/database.js
@@ -1,18 +1,18 @@
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
-    connection: {
-      host: env('PGHOST', '127.0.0.1'),
-      port: env.int('PGPORT', 5432),
-      database: env('PGDATABASE', 'strapi'),
-      user: env('PGUSER', 'strapi'),
-      password: env('PGPASSWORD', 'password'),
-      ssl: env.bool(true),
-    },
-    pool: {
-      min: env.int('DB_POOL_MIN', 2),
-      max: env.int('DB_POOL_MAX', 10),
-    },
-  },
-});
+    module.exports = ({ env }) => ({
+      connection: {
+        client: 'postgres',
+        connection: {
+          host: env('DATABASE_HOST'),
+          port: env.int('DATABASE_PORT', 5432),
+          database: env('DATABASE_NAME'),
+          user: env('DATABASE_USERNAME'),
+          password: env('DATABASE_PASSWORD'),
+          schema: env('DATABASE_SCHEMA', 'public'), // Not required
+          ssl: {
+            rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+          },
+        },
+        debug: false,
+      },
+    });
