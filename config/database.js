9f2abdb67749496458f4d74b15459e09fe8fd513
mohamed-magdy-1 <King44123no@gamil.com>
2025-01-07 11:37:40 +0200

    Updated

--- config/database.js
+++ config/database.js
@@ -1,18 +1,15 @@
-
-    module.exports = ({ env }) => ({
-      connection: {
-        client: 'postgres',
-        connection: {
-          host: env('DATABASE_HOST'),
-          port: env.int('DATABASE_PORT', 5432),
-          database: env('DATABASE_NAME'),
-          user: env('DATABASE_USERNAME'),
-          password: env('DATABASE_PASSWORD'),
-          schema: env('DATABASE_SCHEMA', 'public'), // Not required
-          ssl: {
-            rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
-          },
-        },
-        debug: false,
-      },
-    });
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
+      schema: env('DATABASE_SCHEMA', 'public'), // Not required
+      ssl: env.bool('DATABASE_SSL', false) ? { rejectUnauthorized: false } : false, // Conditional SSL
+    },
+    debug: false,
+  },
+});
