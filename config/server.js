31f3d27394a5288a0a8ace7a8f2ae5863b3f9348
mohamed-magdy-1 <King44123no@gamil.com>
2025-07-15 22:57:14 +0300

    update

--- config/server.js
+++ config/server.js
@@ -7,4 +7,14 @@ module.exports = ({ env }) => ({
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
+  cors: {
+    // origin: ['https://swordmanhwa.site/', 'https://www.swordmanhwa.site/'],
+    // Uncomment the above line and comment the next line to restrict CORS to specific domains
+    // For development, you can allow all origins
+    enabled: true,
+     origin: ['*'], // أثناء التطوير، استبدلها لاحقًا بموقعك للإنتاج
+    headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    credentials: true,
+  },
 });
