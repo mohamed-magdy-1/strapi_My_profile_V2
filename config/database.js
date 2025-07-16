7751b666991cdf5b0fb1869b1448bf5f1cb181bb
mohamed-magdy-1 <King44123no@gamil.com>
2025-07-16 20:34:33 +0300

    update DATABASE_POOL

--- config/database.js
+++ config/database.js
@@ -26,19 +26,24 @@ module.exports = ({ env }) => {
         max: env.int('DATABASE_POOL_MAX', 10),
       },
     },
-    postgres: {
-      connection: {
-        host: env('DATABASE_HOST'),
-        port: env.int('DATABASE_PORT'),
-        database: env('DATABASE_NAME'),
-        user: env('DATABASE_USERNAME'),
-        password: env('DATABASE_PASSWORD'),
-        ssl: env.bool('DATABASE_SSL', false) && {
-          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', false),
-        },
-      },
-      debug: false,
+postgres: {
+  connection: {
+    host: env('DATABASE_HOST'),
+    port: env.int('DATABASE_PORT'),
+    database: env('DATABASE_NAME'),
+    user: env('DATABASE_USERNAME'),
+    password: env('DATABASE_PASSWORD'),
+    ssl: env.bool('DATABASE_SSL', false) && {
+      rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', false),
     },
+  },
+  pool: {
+    min: env.int('DATABASE_POOL_MIN', 2),
+    max: env.int('DATABASE_POOL_MAX', 10),
+  },
+  debug: false,
+},
+
     sqlite: {
       connection: {
         filename: path.join(
